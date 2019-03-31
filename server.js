993bd4ed06de4e436f6a22e769973100187c5459
kgideon <kgideon.dhfs@gmail.com>
2019-03-31 15:02:34 -0400

    connecting to db

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'markedone',
+        host: 'postgresql-globular-15909',
+        user: 'kgideon',
+        password: '',
         database: 'smart-brain'
     }
 });
